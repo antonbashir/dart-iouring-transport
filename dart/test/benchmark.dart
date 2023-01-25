07f1418125ef0528c32fe0b011f565fdbfdd24df
anton.bashirov <anton.sh.local@gmail.com>
2023-01-25 21:31:55 +0200

    bench

--- dart/test/benchmark.dart
+++ dart/test/benchmark.dart
@@ -9,37 +9,41 @@ import 'package:iouring_transport/transport/transport.dart';
 Future<void> main(List<String> args) async {
   final serverTransport = Transport(TransportDefaults.configuration(), TransportDefaults.loop())..initialize();
   final clientTransport = Transport(TransportDefaults.configuration(), TransportDefaults.loop())..initialize();
+  final done = Completer();
 
   var received = 0;
   var sent = 0;
   var seconds = 10;
-  var stop = false;
+  var stopChannels = false;
 
   serverTransport.connection().bind("0.0.0.0", 1234, TransportDefaults.channel()).listen((serverChannel) async {
     serverChannel.stringInput.listen((event) => received++);
-    while (!stop) {
+    while (!stopChannels) {
       await Future.delayed(Duration.zero);
       serverChannel.queueRead();
       serverChannel.queueWriteString("from server");
     }
     serverChannel.stop();
+    done.complete();
   });
 
   clientTransport.connection().connect("127.0.0.1", 1234, TransportDefaults.channel()).listen((clientChannel) async {
     clientChannel.stringInput.listen((event) => sent++);
-    while (!stop) {
+    while (!stopChannels) {
       await Future.delayed(Duration.zero);
       clientChannel.queueRead();
       clientChannel.queueWriteString("from client");
     }
     clientChannel.stop();
+    done.complete();
   });
 
   await Future.delayed(Duration(seconds: seconds));
-  stop = true;
+  stopChannels = true;
+  await done.future;
   serverTransport.close();
   clientTransport.close();
 
   print("received RPS: ${received / seconds}");
-  print("sent RPS: ${received / seconds}");
+  print("sent RPS: ${sent / seconds}");
 }
