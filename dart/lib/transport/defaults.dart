14311374b9f2ae5514c19397000d4d63f50e2f41
anton.bashirov <anton.sh.local@gmail.com>
2023-03-24 15:32:23 +0200

    fixes

--- dart/lib/transport/defaults.dart
+++ dart/lib/transport/defaults.dart
@@ -6,20 +6,20 @@ class TransportDefaults {
 
   static TransportConfiguration transport() => TransportConfiguration(
         logLevel: TransportLogLevel.debug,
-        listenerIsolates: 1,
+        listenerIsolates: 2,
         workerInsolates: 2,
       );
 
   static TransportListenerConfiguration listener() => TransportListenerConfiguration(
         ringSize: 16384,
-        ringFlags: ringSetupSqpoll,
+        ringFlags: 0,
       );
 
   static TransportWorkerConfiguration worker() => TransportWorkerConfiguration(
         buffersCount: 4096,
         bufferSize: 4096,
         ringSize: 16384,
-        ringFlags: ringSetupSqpoll,
+        ringFlags: 0,
       );
 
   static TransportClientConfiguration client() => TransportClientConfiguration(
