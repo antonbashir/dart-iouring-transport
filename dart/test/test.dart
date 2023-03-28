871a6167e50fe3a0046a2d308359e70fc4019a08
anton.bashirov <anton.sh.local@gmail.com>
2023-03-29 02:34:48 +0300

    fix cmake

--- dart/test/test.dart
+++ dart/test/test.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:isolate';
 
+import 'package:iouring_transport/transport/constants.dart';
 import 'package:iouring_transport/transport/defaults.dart';
 import 'package:iouring_transport/transport/model.dart';
 import 'package:iouring_transport/transport/transport.dart';
@@ -9,20 +10,31 @@ import 'package:test/test.dart';
 
 void main() {
   group("[base]", () {
-    echo(1, 1, 1);
-    echo(2, 2, 1);
-    echo(4, 4, 1);
-    echo(4, 4, 128);
+    final echoTestsCount = 4;
+    for (var index = 0; index < echoTestsCount; index++) {
+      echo(index: index, listeners: 1, workers: 1, clients: 1, listenerFlags: 0, workerFlags: ringSetupSqpoll);
+      echo(index: index, listeners: 2, workers: 2, clients: 1, listenerFlags: 0, workerFlags: ringSetupSqpoll);
+      echo(index: index, listeners: 4, workers: 4, clients: 1, listenerFlags: 0, workerFlags: ringSetupSqpoll);
+      echo(index: index, listeners: 4, workers: 4, clients: 128, listenerFlags: 0, workerFlags: ringSetupSqpoll);
+      echo(index: index, listeners: 2, workers: 2, clients: 8, listenerFlags: 0, workerFlags: ringSetupSqpoll);
+    }
   });
 }
 
-void echo(int listeners, int workers, int clients) {
-  test("[listeners = $listeners, workers = $workers, clients = $clients]", () async {
+void echo({
+  required int index,
+  required int listeners,
+  required int workers,
+  required int clients,
+  required int listenerFlags,
+  required int workerFlags,
+}) {
+  test("[index = $index, listeners = $listeners, workers = $workers, clients = $clients]", () async {
     final transport = Transport(
       TransportDefaults.transport().copyWith(listenerIsolates: listeners, workerInsolates: workers),
       TransportDefaults.acceptor(),
-      TransportDefaults.listener(),
-      TransportDefaults.worker(),
+      TransportDefaults.listener().copyWith(ringFlags: listenerFlags),
+      TransportDefaults.worker().copyWith(ringFlags: workerFlags),
       TransportDefaults.client().copyWith(defaultPool: clients),
     );
     final done = ReceivePort();
