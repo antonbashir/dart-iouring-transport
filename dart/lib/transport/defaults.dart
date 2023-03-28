fe9c4f1b6bd8e5abe8e7c782db43faf96c3e2cae
anton.bashirov <anton.sh.local@gmail.com>
2023-03-29 00:25:20 +0300

    refactoring

--- dart/lib/transport/defaults.dart
+++ dart/lib/transport/defaults.dart
@@ -12,14 +12,14 @@ class TransportDefaults {
 
   static TransportListenerConfiguration listener() => TransportListenerConfiguration(
         ringSize: 32768,
-        ringFlags: ringSetupSingleIssuer,
+        ringFlags: ringSetupDeferTaskrun | ringSetupTaskrunFlag | ringSetupSingleIssuer,
       );
 
   static TransportWorkerConfiguration worker() => TransportWorkerConfiguration(
         buffersCount: 4096,
         bufferSize: 4096,
         ringSize: 32768,
-        ringFlags: ringSetupSqpoll | ringSetupSingleIssuer,
+        ringFlags: ringSetupSqpoll | ringSetupDeferTaskrun | ringSetupTaskrunFlag | ringSetupSingleIssuer,
       );
 
   static TransportClientConfiguration client() => TransportClientConfiguration(
