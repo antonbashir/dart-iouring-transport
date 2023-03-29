daa7d23fd4a58fc6a1968533cec81044c1897e27
anton.bashirov <anton.sh.local@gmail.com>
2023-03-29 20:33:35 +0300

    fixes

--- dart/test/benchmark.dart
+++ dart/test/benchmark.dart
@@ -28,9 +28,7 @@ Future<void> main(List<String> args) async {
       final worker = TransportWorker(input);
       await worker.initialize();
       await worker.serve((channel) => channel.read(), (stream) => stream.listen((event) => event.respond(fromServer)));
-      print("Served");
       final connector = await worker.connect(TransportUri.tcp("127.0.0.1", 12345), pool: 256);
-      print("Connected");
       var count = 0;
       final time = Stopwatch();
       time.start();
@@ -38,11 +36,10 @@ Future<void> main(List<String> args) async {
         count += (await Future.wait(connector.map((client) => client.write(fromServer).then((value) => client.read()).then((value) => value.release())))).length;
         if (time.elapsed.inSeconds >= 10) break;
       }
-      print("Send $count");
       worker.transmitter!.send(count);
     },
   );
   final count = await receiver.take(TransportDefaults.transport().workerInsolates).reduce((previous, element) => previous + element);
-  print("Done: ${count / 10}");
+  print("RPS: ${count / 10}");
   exit(0);
 }
