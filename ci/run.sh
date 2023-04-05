57adc7716a7a268c8efa9cf8b3b298f3b751cdc7
anton.bashirov <anton.sh.local@gmail.com>
2023-04-05 18:30:14 +0300

    fix

--- ci/run.sh
+++ ci/run.sh
@@ -9,7 +9,7 @@ set -x
 ssh -o "StrictHostKeyChecking=no" "runner@$IP" uname -a
 scp -r $(pwd) "runner@$IP:/home/runner"
 ssh "runner@$IP" "sudo apt-get update && sudo apt install -y -q wget apt-transport-https && wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/dart.gpg && sudo echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list && sudo apt-get update && sudo apt-get install -y -q dart"
-ssh "runner@$IP" "cd dart-iouring-transport && cd dart && dart pub get && dart run test/test.dart"
+ssh "runner@$IP" "cd dart-iouring-transport && cd dart && dart pub get && dart compile exe test/test.dart && test/test.exe"
 
 if [ $? -eq 0 ]
 then
