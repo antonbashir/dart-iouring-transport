bc49b9e6d69f6dbfa69a1001e4090369128400d2
anton.bashirov <anton.sh.local@gmail.com>
2023-04-05 16:48:20 +0300

    fix

--- ci/run.sh
+++ ci/run.sh
@@ -10,9 +10,7 @@ ssh -o "StrictHostKeyChecking=no" "runner@$IP" uname -a
 
 scp -r $(pwd) "runner@$IP:/home/runner"
 
-ssh "runner@$IP" "pwd; ls"
-
-ssh "runner@$IP" df -h
+ssh "runner@$IP" "cd dart-iouring-transport && cmake -DCMAKE_BUILD_TYPE=Release . && make -j && cd dart && dart pub get && dart run test/test.dart"
 
 sudo virsh shutdown inner
 until sudo virsh domstate inner | grep shut; do
