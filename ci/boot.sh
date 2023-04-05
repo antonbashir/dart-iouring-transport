eac6dbc5c83041b4324a402563514a3724ebf4ca
anton.bashirov <anton.sh.local@gmail.com>
2023-04-05 14:29:55 +0300

    cache

--- ci/boot.sh
+++ ci/boot.sh
@@ -4,10 +4,6 @@ OS_TYPE="ubuntu-22.10-cloud"
 VM_NAME="inner"
 DISK_IMAGE="ubuntu-22.10-server-cloudimg-amd64.img"
 
-if [ ! -f "$DISK_IMAGE" ]; then
-  wget https://cloud-images.ubuntu.com/releases/22.10/release/ubuntu-22.10-server-cloudimg-amd64.img
-fi
-
 virt-install --import \
     --name "$VM_NAME" \
     --vcpu 2 \
---
+++ ci/download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://cloud-images.ubuntu.com/releases/22.10/release/ubuntu-22.10-server-cloudimg-amd64.img
