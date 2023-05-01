f9c47961a511820a0d3b639b02447d74d6857b23
anton.bashirov <anton.sh.local@gmail.com>
2023-05-01 20:45:54 +0300

    fixes...

--- dart/test/buffers.dart
+++ dart/test/buffers.dart
@@ -52,6 +52,50 @@ void testTcpBuffers() {
   });
 }
 
+void testBuffersOverflow() {
+  test("(overflow)", () async {
+    final transport = Transport(TransportDefaults.transport(), TransportDefaults.listener(), TransportDefaults.inbound(), TransportDefaults.outbound());
+    final done = ReceivePort();
+    transport.run(transmitter: done.sendPort, (input) async {
+      final worker = TransportWorker(input);
+      await worker.initialize();
+
+      var server = worker.servers.tcp(InternetAddress("0.0.0.0"), 12345, (connection) => connection.writeSingle(Generators.request()));
+      var clients = await worker.clients.tcp(InternetAddress("127.0.0.1"), 12345);
+      await clients.select().read().then((value) => value.release());
+
+      if (worker.inboundBuffers.used() != 0) throw TestFailure("actual: ${worker.inboundBuffers.used()}");
+      if (worker.outboundBuffers.used() != 0) throw TestFailure("actual: ${worker.outboundBuffers.used()}");
+
+      await server.close();
+      await clients.close();
+
+      if (worker.servers.registry.serverConnections.isNotEmpty) throw TestFailure("serverConnections isNotEmpty");
+      if (worker.servers.registry.servers.isNotEmpty) throw TestFailure("servers isNotEmpty");
+      if (worker.clients.registry.clients.isNotEmpty) throw TestFailure("clients isNotEmpty");
+
+      server = worker.servers.tcp(InternetAddress("0.0.0.0"), 12345, (connection) => connection.writeMany(Generators.requestsUnordered(8)));
+      clients = await worker.clients.tcp(InternetAddress("127.0.0.1"), 12345);
+      await clients.select().read().then((value) => value.release());
+
+      if (worker.inboundBuffers.used() != 0) throw TestFailure("actual: ${worker.inboundBuffers.used()}");
+      if (worker.outboundBuffers.used() != 0) throw TestFailure("actual: ${worker.outboundBuffers.used()}");
+
+      await server.close();
+      await clients.close();
+
+      if (worker.servers.registry.serverConnections.isNotEmpty) throw TestFailure("serverConnections isNotEmpty");
+      if (worker.servers.registry.servers.isNotEmpty) throw TestFailure("servers isNotEmpty");
+      if (worker.clients.registry.clients.isNotEmpty) throw TestFailure("clients isNotEmpty");
+
+      worker.transmitter!.send(null);
+    });
+    await done.take(TransportDefaults.transport().workerInsolates).toList();
+    done.close();
+    await transport.shutdown();
+  });
+}
+
 void testUdpBuffers() {
   test("(udp)", () async {
     final transport = Transport(TransportDefaults.transport(), TransportDefaults.listener(), TransportDefaults.inbound(), TransportDefaults.outbound());
