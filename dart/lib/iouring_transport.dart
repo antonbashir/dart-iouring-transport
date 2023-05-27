ce51ed4277817ff8fcb9003e922d4f9d9f13542b
anton.bashirov <anton.sh.local@gmail.com>
2023-05-27 23:10:45 +0300

    fixes

--- dart/lib/iouring_transport.dart
+++ dart/lib/iouring_transport.dart
@@ -13,7 +13,7 @@ export 'package:iouring_transport/transport/server/factory.dart' show TransportS
 
 export 'package:iouring_transport/transport/client/configuration.dart'
     show TransportTcpClientConfiguration, TransportUdpClientConfiguration, TransportUnixDatagramClientConfiguration, TransportUnixStreamClientConfiguration;
-export 'package:iouring_transport/transport/client/provider.dart' show TransportDatagramClient;
+export 'package:iouring_transport/transport/client/provider.dart' show TransportDatagramClient, TransportClientConnection;
 export 'package:iouring_transport/transport/client/factory.dart' show TransportClientsFactory;
 
 export 'package:iouring_transport/transport/file/provider.dart' show TransportFile;
