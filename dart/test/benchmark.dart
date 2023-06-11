5db6abe664107d4621badd7a9db29ecd1dcf47b2
anton.bashirov <anton.sh.local@gmail.com>
2023-06-12 00:48:53 +0300

    add dart io benchmark

--- dart/test/benchmark.dart
+++ dart/test/benchmark.dart
@@ -9,10 +9,11 @@ import 'package:iouring_transport/transport/transport.dart';
 import 'package:iouring_transport/transport/worker.dart';
 
 Future<void> main(List<String> args) async {
-  await _benchTcp();
+  await _benchMyTcp();
+  await _benchDartTcp();
 }
 
-Future<void> _benchTcp() async {
+Future<void> _benchMyTcp() async {
   final transport = Transport();
   final encoder = Utf8Encoder();
   final fromServer = encoder.convert("from server\n");
@@ -49,10 +50,50 @@ Future<void> _benchTcp() async {
         client.writeSingle(fromServer);
       }
       await Future.delayed(Duration(seconds: 10));
-      print("RPS: ${count / 10}");
+      print("My RPS: ${count / 10}");
     }, transport.worker(TransportDefaults.worker()));
   }
 
   await Future.delayed(Duration(seconds: 15));
   await transport.shutdown();
+}
+
+Future<void> _benchDartTcp() async {
+  final encoder = Utf8Encoder();
+  final fromServer = encoder.convert("from server\n");
+
+  for (var i = 0; i < 2; i++) {
+    Isolate.spawn((_) async {
+      ServerSocket.bind(InternetAddress("127.0.0.1"), 2345, shared: true).then((server) {
+        server.listen((client) {
+          client.listen((event) {
+            client.write(fromServer);
+          });
+        });
+      });
+    }, null);
+  }
+  await Future.delayed(Duration(seconds: 1));
+  for (var i = 0; i < 2; i++) {
+    Isolate.spawn((_) async {
+      final sockets = <Socket>[];
+      for (var i = 0; i < 256; i++) {
+        sockets.add(await Socket.connect("127.0.0.1", 2345));
+      }
+      var count = 0;
+      final time = Stopwatch();
+      time.start();
+      for (var client in sockets) {
+        client.listen((element) {
+          count++;
+          client.write(fromServer);
+        });
+        client.write(fromServer);
+      }
+      await Future.delayed(Duration(seconds: 10));
+      print("Dart RPS: ${count / 10}");
+    }, null);
+  }
+
+  await Future.delayed(Duration(seconds: 15));
 }
