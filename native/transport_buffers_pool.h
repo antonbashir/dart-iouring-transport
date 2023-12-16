92a6a36313bd041bd92d466f7e088a6b1d9848db
anton.bashirov <anton.sh.local@gmail.com>
2023-12-16 18:34:02 +0200

    refactoring

--- native/transport_buffers_pool.h
+++ native/transport_buffers_pool.h
@@ -1,41 +1,45 @@
 #ifndef TRANSPORT_BUFFERS_POOL_INCLUDED
 #define TRANSPORT_BUFFERS_POOL_INCLUDED
 
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 #include "common/common.h"
 #include "transport_constants.h"
 
 struct transport_buffers_pool
 {
-  int32_t *ids;
-  size_t count;
-  size_t size;
+    int32_t* ids;
+    size_t count;
+    size_t size;
 };
 
-static inline int transport_buffers_pool_create(struct transport_buffers_pool *pool, size_t size)
+static inline int transport_buffers_pool_create(struct transport_buffers_pool* pool, size_t size)
 {
-  pool->size = size;
-  pool->count = 0;
-  pool->ids = (int32_t *)malloc(size * sizeof(int32_t));
-  memset(pool->ids, 0, size * sizeof(int32_t));
-  return (pool->ids == NULL ? -1 : 0);
+    pool->size = size;
+    pool->count = 0;
+    pool->ids = (int32_t*)malloc(size * sizeof(int32_t));
+    memset(pool->ids, 0, size * sizeof(int32_t));
+    return (pool->ids == NULL ? -1 : 0);
 }
 
-static inline void transport_buffers_pool_destroy(struct transport_buffers_pool *pool)
+static inline void transport_buffers_pool_destroy(struct transport_buffers_pool* pool)
 {
-  free(pool->ids);
-  pool->ids = NULL;
+    free(pool->ids);
+    pool->ids = NULL;
 }
 
-static inline void transport_buffers_pool_push(struct transport_buffers_pool *pool, int32_t id)
+static inline void transport_buffers_pool_push(struct transport_buffers_pool* pool, int32_t id)
 {
-  pool->ids[pool->count++] = id;
+    pool->ids[pool->count++] = id;
 }
 
-static inline int32_t transport_buffers_pool_pop(struct transport_buffers_pool *pool)
+static inline int32_t transport_buffers_pool_pop(struct transport_buffers_pool* pool)
 {
-  if (unlikely(pool->count == 0))
-    return TRANSPORT_BUFFER_USED;
-  return pool->ids[--pool->count];
+    if (unlikely(pool->count == 0))
+        return TRANSPORT_BUFFER_USED;
+    return pool->ids[--pool->count];
 }
 
 #endif
